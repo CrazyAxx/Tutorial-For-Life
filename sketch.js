af2c9736c8e50152d835c65254b4d06b6cae1517
CrazyAxx <244503@bwls.net>
2016-10-14 11:51:20 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,14 @@
 function setup() {
   createCanvas( windowWidth , windowHeight );
-  background( 0 , 0 , 0 );
 }
 
 function draw() {
   ellipse( mouseX , mouseY , 20 , 20 );
-  fill( 255 , 165 , 0 , 50 );
+  fill( 255 , 165 , 0 );
   noStroke(); 
   
   ellipse( mouseX , mouseY , 10 , 10 );
-  fill( 255 , 0 , 0 , 50 );
+  fill( 255 , 0 , 0 );
   noStroke();
   
 
