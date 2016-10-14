3a3c3cddd9b379a7704b8743589c6e727c6fa02a
CrazyAxx <244503@bwls.net>
2016-10-14 11:47:01 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,5 +5,10 @@ function setup() {
 
 function draw() {
   ellipse( mouseX , mouseY , 10 , 10 );
+  fill( 255 , 0 , 0 );
+  noStroke();
   
+  ellipse( mouseX , mouseY , 10 , 10 );
+  fill( 255 , 165 , 0 );
+  noStroke();
 }
