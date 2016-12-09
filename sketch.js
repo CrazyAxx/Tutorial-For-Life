6e110c6cde8b2d94ad013d6b2109f4f58aa0d4a2
Collin Bates <244503@bwls.net>
2016-12-08 19:20:45 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-
+  createCanvas( windowWidth , windowHeight);
+  
+  bakcground( 0 , 0 , 0 );
+  
 }
 
 function draw() {
-
+  fill( 255 , 0 , 0 );
+  ellipse( mouseX , mouseY , 10 );
+  fill( 255 , 157 , 0 );
+  ellipse( mouseX , mouseY , 10 );
+  fill( 255 , 255 , 0 );
+  ellipse( mouseX , mouseY , 10 );
+  fill( 0 , 255 , 0 );
+  ellipse( mouseX , mouseY , 10 );
+  fill( 0 , 0 , 255 );
+  ellipse( mouseX , mouseY , 10 );
+  fill( 255 , 0 , 255 );
+  ellipse( mouseX , mouseY , 10 );
+  
 }
