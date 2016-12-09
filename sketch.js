dce8273bb955633b821474444d25469c6c5d6a66
Collin Bates <244503@bwls.net>
2016-12-08 19:25:13 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,17 +5,17 @@ function setup() {
 }
 
 function draw() {
-  fill( 255 , 0 , 0 );
-  ellipse( mouseX , mouseY , 60 );
-  fill( 255 , 157 , 0 );
-  ellipse( mouseX , mouseY , 50 );
-  fill( 255 , 255 , 0 );
-  ellipse( mouseX , mouseY , 40 );
-  fill( 0 , 255 , 0 );
-  ellipse( mouseX , mouseY , 30 );
-  fill( 0 , 0 , 255 );
-  ellipse( mouseX , mouseY , 20 );
   fill( 255 , 0 , 255 );
   ellipse( mouseX , mouseY , 10 );
+  fill( 0 , 0 , 255 );
+  ellipse( mouseX , mouseY , 20 );
+  fill( 0 , 255 , 0 );
+  ellipse( mouseX , mouseY , 30 );
+  fill( 255 , 255 , 0 );
+  ellipse( mouseX , mouseY , 40 );
+  fill( 255 , 157 , 0 );
+  ellipse( mouseX , mouseY , 50 );
+  fill( 255 , 0 , 0 );
+  ellipse( mouseX , mouseY , 60 );
   
 }
